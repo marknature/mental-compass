a1a0b40147e6f6c595d88cf531c0ce72f09ed1b7
icep0ps <tapsmuko@gmail.com>
2025-03-11 02:06:19 +0200

    feat: added journals page

---
+++ app/journals/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { MoreHorizontal, Plus } from "lucide-react";
+
+export default function JournalPage() {
+  return (
+    <>
+      <div className="space-y-4">
+        <h5 className="text-sm text-muted-foreground">
+          Edited Wednesday 5 July
+        </h5>
+        <h1 className="text-4xl mb-2 text-foreground font-semibold">
+          Thanking God for the small things
+        </h1>
+      </div>
+      <div className="grid grid-cols-4 gap-2 mb-6">
+        <Button variant="secondary" className="text-sm rounded-lg">
+          Podcasts
+        </Button>
+        <Button variant="secondary" className="text-sm rounded-lg">
+          Podcasts
+        </Button>
+        <Button variant="outline" size={"icon"} className="text-sm rounded-lg">
+          <Plus />
+        </Button>
+      </div>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitatiLorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati
+      </p>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitatiLorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitati
+      </p>
+    </>
+  );
+}
---
+++ app/journals/page.tsx
@@ -0,0 +1,108 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { MoreHorizontal, Plus } from "lucide-react";
+import Link from "next/link";
+
+export default function Journal() {
+  const journals = [
+    {
+      title: "Thanking God for the small things",
+      content: `
+            Always thank God for the small things in life, waking up being
+            healthy having food on the table. Even when you have little or
+            nothing as long as you got breath in your lungs you got something to
+            thank God for and a mission you have to complete. Use the little you
+            have to get more and God will reward you with much more
+      `,
+      url: "/journals/1",
+    },
+    {
+      title: "Thanking God for the small things",
+      content: `
+            Always thank God for the small things in life, waking up being
+            healthy having food on the table. Even when you have little or
+            nothing as long as you got breath in your lungs you got something to
+            thank God for and a mission you have to complete. Use the little you
+            have to get more and God will reward you with much more
+      `,
+      url: "/journals/2",
+    },
+    {
+      title: "Thanking God for the small things",
+      content: `
+            Always thank God for the small things in life, waking up being
+            healthy having food on the table. Even when you have little or
+            nothing as long as you got breath in your lungs you got something to
+            thank God for and a mission you have to complete. Use the little you
+            have to get more and God will reward you with much more
+      `,
+      url: "/journals/3",
+    },
+    {
+      title: "Thanking God for the small things",
+      content: `
+            Always thank God for the small things in life, waking up being
+            healthy having food on the table. Even when you have little or
+            nothing as long as you got breath in your lungs you got something to
+            thank God for and a mission you have to complete. Use the little you
+            have to get more and God will reward you with much more
+      `,
+      url: "/journals/4",
+    },
+  ];
+  return (
+    <>
+      <h1 className="text-2xl mb-2 text-foreground">
+        Journal
+        <p className="text-sm text-muted-foreground">
+          Get professional help from the Africa university counselor
+        </p>
+      </h1>
+      <div className="mb-6 flex gap-2">
+        <Input
+          type="search"
+          placeholder="Search for jornals..."
+          className="w-full text-sm"
+        />
+        <Button size={"icon"} variant={"outline"}>
+          <MoreHorizontal />
+        </Button>
+      </div>
+      <div className="grid grid-cols-4 gap-2 mb-6">
+        <Button variant="secondary" className="text-sm rounded-lg">
+          Podcasts
+        </Button>
+        <Button variant="secondary" className="text-sm rounded-lg">
+          Podcasts
+        </Button>
+        <Button variant="secondary" className="text-sm rounded-lg">
+          Podcasts
+        </Button>
+        <Button variant="secondary" className="text-sm rounded-lg">
+          Podcasts
+        </Button>
+      </div>
+
+      <div className="grid grid-cols-2 gap-4">
+        {journals.map(({ url, content, title }) => {
+          return (
+            <Link href={url} key={url}>
+              <Card className="p-4 space-y-2 bg-border border-none">
+                <CardTitle>{title}</CardTitle>
+                <CardContent className="p-0 text-sm line-clamp-3">
+                  {content}
+                </CardContent>
+              </Card>
+            </Link>
+          );
+        })}
+      </div>
+
+      <Button size={"icon"} className="z-40 bottom-40 absolute right-5">
+        <Plus size={"50"} />
+      </Button>
+    </>
+  );
+}

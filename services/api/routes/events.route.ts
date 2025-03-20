711690cffa5fdfd7558e913adfa8373f4e676eef
icep0ps <tapsmuko@gmail.com>
2025-03-21 01:24:22 +0200

    breaking: changed file structure to fix workspaces

--- services/api/routes/events.route.ts
+++ services/api/routes/events.route.ts
@@ -1,9 +1,20 @@
-// routes/events.ts
 import express from "express";
+import { validate } from "../middlewares/validate";
+import { eventSchema } from "@models/events.schema";
+import {
+  getAllEvents,
+  getEventById,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} from "@controllers/events.controller";
+
 const router = express.Router();
 
-// Events and challenges routes
-router.post("/", (req, res) => {});
-router.get("/", (req, res) => {});
+router.get("/", getAllEvents);
+router.get("/:id", getEventById);
+router.post("/", validate(eventSchema), createEvent);
+router.put("/:id", validate(eventSchema), updateEvent);
+router.delete("/:id", deleteEvent);
 
 export default router;

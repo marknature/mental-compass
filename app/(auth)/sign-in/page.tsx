1b25408e1e23db61180f33619649f2de2a61f5c1
icep0ps <tapsmuko@gmail.com>
2025-04-06 23:45:20 +0200

    bugfix: no suspense boundaries on auth pages

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,16 @@
+"use client";
+
+import Loading from "@/app/_components/loading";
+import { Suspense } from "react";
 import { LoginForm } from "../_components/login-form";
 
 export default function LoginPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <LoginForm />
+        <Suspense fallback={<Loading title="sign in" />}>
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
   );
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+import { Suspense } from "react";
 import { SignupForm } from "../_components/sign-up-form";
+import Loading from "@/app/_components/loading";
 
 export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-background p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <SignupForm />
+        <Suspense fallback={<Loading title="sign up" />}>
+          <SignupForm />
+        </Suspense>
       </div>
     </div>
   );
